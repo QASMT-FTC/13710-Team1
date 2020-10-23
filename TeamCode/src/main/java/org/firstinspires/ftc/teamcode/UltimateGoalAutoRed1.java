7c57854eff196818682a7d1e5738879a64ada420
MagnusBlums <magnus.blums@gmail.com>
2020-10-23 15:09:30 +1000

    Update UltimateGoalAutoRed1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoalAutoRed1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoalAutoRed1.java
@@ -2,9 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+@Disabled
 @Autonomous(name="UltimateGoalAutoRed1", group="Linear OpMode")
 public class UltimateGoalAutoRed1 extends LinearOpMode {
 
